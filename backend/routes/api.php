44b700bd5e90b00eebcc3d62ca8da94c86bed6ca
Fayad ROUFAI <fayadroufai241@gmail.com>
2026-02-23 23:59:27 +0100

    feat: implementation DashboardController

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,6 +7,9 @@ use App\Http\Controllers\Api\ServiceController;
 use App\Http\Controllers\Api\SlotController;
 use App\Http\Controllers\Api\VehiculeController;
 use App\Http\Controllers\Api\ReservationController;
+use App\Http\Controllers\Api\Admin\DashboardController;
+use App\Http\Controllers\Api\Admin\PlanningController;
+use App\Http\Controllers\Api\Admin\ClientController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -56,4 +59,17 @@ Route::middleware('auth:api')->group(function () {
         Route::patch('/{reservation}/status', [ReservationController::class, 'updateStatus'])
             ->middleware('role:employee,manager,admin');
     });
+
+});
+
+
+Route::middleware(['auth:api', 'role:employee,manager,admin'])->prefix('admin')->group(function () {
+
+    // Dashboard
+    Route::prefix('dashboard')->group(function () {
+        Route::get('/', [DashboardController::class, 'index']);
+        Route::get('/revenue', [DashboardController::class, 'revenue']);
+        Route::get('/services', [DashboardController::class, 'services']);
+    });
+
 });
